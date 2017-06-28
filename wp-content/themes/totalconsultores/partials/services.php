1374053fd20b21e0882a4db4a3b6fa467fc63111
Jose Perez <joseluis@watson.pe>
2017-06-28 10:13:26 -0500

    Home page service WP

--- wp-content/themes/totalconsultores/partials/services.php
+++ wp-content/themes/totalconsultores/partials/services.php
@@ -1,14 +1,33 @@
-<section class="Services">
-  <article class="Services-item">
-    <img src="<?php echo IMAGES; ?>/cultural.jpg" alt="" class="Services-bg img-responsive" />
-    <aside class="Services-title text-uppercase"><h3 class="text-center">Cultural.</h3></aside>
-  </article>
-  <article class="Services-item">
-    <img src="<?php echo IMAGES; ?>/educacion.jpg" alt="" class="Services-bg img-responsive" />
-    <aside class="Services-title text-uppercase"><h3 class="text-center">Educación.</h3></aside>
-  </article>
-  <article class="Services-item">
-    <img src="<?php echo IMAGES; ?>/turistico.jpg" alt="" class="Services-bg img-responsive" />
-    <aside class="Services-title text-uppercase"><h3 class="text-center">Turístico.</h3></aside>
-  </article>
-</section>
+<?php
+  $pageParent = get_page_by_title('Servicios');
+
+  $args = [
+    'post_type' => 'page',
+    'order' => 'ASC',
+    'orderby' => 'menu_order',
+    'posts_per_page' => -1
+  ];
+
+  $services = new WP_Query($args);
+
+  if ($services->have_posts()) :
+?>
+  <section class="Services hidden-xs hidden-sm">
+    <?php while ($services->have_posts()) : ?>
+      <?php $services->the_post(); ?>
+      <?php if (has_post_thumbnail()) : ?>
+        <article class="Services-item">
+          <?php the_post_thumbnail('full', [
+              'class' => 'img-responsive center-block',
+              'alt' => get_the_title()
+            ]);
+          ?>
+          <aside class="Services-title text-uppercase">
+            <h3 class="text-center"><?php the_title(); ?>.</h3>
+          </aside>
+        </article>
+      <?php endif; ?>
+    <?php endwhile; ?>
+  </section>
+<?php endif; ?>
+<?php wp_reset_postdata(); ?>
